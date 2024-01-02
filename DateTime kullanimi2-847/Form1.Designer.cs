f0cf9979b913a4342c917d84f165e8089eb9af28
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2024-01-02 10:56:31 +0300

    Add files via upload

---
+++ DateTime kullanimi2-847/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DateTime_kullanimi2_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblSuan = new System.Windows.Forms.Label();
+            this.lblBugun = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(50, 48);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(114, 34);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblSuan
+            // 
+            this.lblSuan.AutoSize = true;
+            this.lblSuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSuan.Location = new System.Drawing.Point(223, 48);
+            this.lblSuan.Name = "lblSuan";
+            this.lblSuan.Size = new System.Drawing.Size(51, 24);
+            this.lblSuan.TabIndex = 1;
+            this.lblSuan.Text = "şuan";
+            // 
+            // lblBugun
+            // 
+            this.lblBugun.AutoSize = true;
+            this.lblBugun.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBugun.Location = new System.Drawing.Point(223, 81);
+            this.lblBugun.Name = "lblBugun";
+            this.lblBugun.Size = new System.Drawing.Size(65, 24);
+            this.lblBugun.TabIndex = 2;
+            this.lblBugun.Text = "bugün";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBugun);
+            this.Controls.Add(this.lblSuan);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblSuan;
+        private System.Windows.Forms.Label lblBugun;
+    }
+}
+
---
+++ DateTime kullanimi2-847/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTime_kullanimi2_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+
+            lblSuan.Text = "Şuan  : " + DateTime.Now;
+            lblBugun.Text = "Bugün: " + DateTime.Today;
+        }
+    }
+}
